95716e3a5efff573d13d68083395e366bab5d0ce
cahgaul <achmadfauziharismawan@gmail.com>
2017-05-24 20:23:35 +0700

    fix login with basic auth

--- app/src/main/java/in/buka/app/ui/activities/ActivityFeedActivity.java
+++ app/src/main/java/in/buka/app/ui/activities/ActivityFeedActivity.java
@@ -5,22 +5,11 @@
 
 package in.buka.app.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.MenuItem;
 
-import android.view.View;
-import android.widget.FrameLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
